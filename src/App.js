136c95be341d605526cb0a9c4d88a88a009236a1
Gabriela Moreno <gabrielaxmoreno@gmail.com>
2020-04-07 17:40:18 -0700

    landing page completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
+import burger from './images/burger.jpeg';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header" style={{ backgroundImage:`url(${burger})` }}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Bruce's Diner
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <Button variant="secondary">Login</Button>
       </header>
     </div>
   );
